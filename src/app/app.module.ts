b7b95eabe46dd4e56ff298dcb3429943ee294db1
Josh <josh@iserveproducts.com>
2017-08-29 21:38:55 -0600

    Adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdInputModule, MdMenuModule, MdSidenavModule, MdCardModule, MdGridListModule, MdTabsModule, MdButtonModule, MdIconModule, MdProgressSpinnerModule, MdDialogModule, MdSnackBarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdInputModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdCardModule,
+    MdGridListModule,
+    MdTabsModule,
+    MdButtonModule,
+    MdIconModule,
+    MdProgressSpinnerModule,
+    MdDialogModule,
+    MdSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
