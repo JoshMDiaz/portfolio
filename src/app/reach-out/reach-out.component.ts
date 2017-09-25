edb52cca15b8aad1ddef21c3660f58c9e72ce4fd
Josh <josh@iserveproducts.com>
2017-09-25 08:38:48 -0600

    Building out reach-out and about-me

--- src/app/reach-out/reach-out.component.ts
+++ src/app/reach-out/reach-out.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 @Component({
   selector: 'app-reach-out',
@@ -6,8 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reach-out.component.scss']
 })
 export class ReachOutComponent implements OnInit {
+  contact: {} = {
+    jobType: 'New Website'
+  };
 
   constructor() { }
+
+  nameFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(EMAIL_REGEX)
+  ]);
+
+  jobTypes = [
+    {
+      id: 1,
+      value: 'New Website',
+      name: 'Create a new website',
+      selected: true
+    },
+    {
+      id: 2,
+      value: 'Update Website',
+      name: 'Update an existing website',
+      selected: false
+    },
+    {
+      id: 3,
+      value: 'New App',
+      name: 'Create a new app',
+      selected: false
+    }
+  ];
+
+  send(contact) {
+    console.log(contact);
+  }
 
   ngOnInit() {
   }
