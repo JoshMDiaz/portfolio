84cd1e0e0bc55485ec6cbd2dd7c99a13800848cd
Josh Diaz <josh@iserveproducts.com>
2017-11-24 20:18:50 -0700

    Starting work detail page

--- src/app/my-work-detail/my-work-detail.component.ts
+++ src/app/my-work-detail/my-work-detail.component.ts
@@ -12,7 +12,8 @@ export class MyWorkDetailComponent implements OnInit {
     title: string,
     bannerSecondaryText: string,
     logo: string,
-    link: string
+    link: string,
+    description: string
   }>;
   projectId: number;
   currentProject: {};
@@ -34,21 +35,24 @@ export class MyWorkDetailComponent implements OnInit {
         title: 'Taylored Ties',
         bannerSecondaryText: 'Website.&nbsp;&nbsp;eCommerce.&nbsp;&nbsp;Small Business.',
         logo: '../../assets/taylored-ties.png',
-        link: 'http://tayloredties.com'
+        link: 'http://tayloredties.com',
+        description: 'Taylored Ties is slick new way of getting that tie on right the first time. The look of the ties are modern, the prices of the ties are affordable, and the simplicity of the ties are fantastic. This project was fun to be able to showcase these awesome ties.'
       },
       {
         id: 2,
         title: 'Shyanne Diaz',
         bannerSecondaryText: 'Website.&nbsp;&nbsp;Blog.&nbsp;&nbsp;Small Business.',
-        logo: '../../assets/shyanne-diaz.png',
-        link: 'http://shyannediaz.com'
+        logo: '../../assets/shyanne-diaz-no_bg.png',
+        link: 'http://shyannediaz.com',
+        description: 'I helped Shyanne bring her blog design vision to life. She knew what she wanted for the content, but did not know how to display it all. This blog was what she needed to help her start her small business.'
       },
       {
         id: 3,
         title: 'Ronin Dragon Karate',
         bannerSecondaryText: 'Website.&nbsp;&nbsp;Karate.&nbsp;&nbsp;Small Business.',
-        logo: '../../assets/rdk.png',
-        link: 'http://rdkutah.com'
+        logo: '../../assets/rdk-no_bg.png',
+        link: 'http://rdkutah.com',
+        description: 'This karate dojo is small and relatively unknown. By adding a website, Jordan has been able to reach more people and get more students than previously possible. He has also been able to include content that has enriched the experience that his students get outside of the dojo.'
       }
     ];
     this.projectId = this.route.snapshot.params.id;
